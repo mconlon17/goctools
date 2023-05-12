122a88f111e2b071e0b861305fc3733c73644cea
Mike Conlon <mconlon@ufl.edu>
2023-05-12 17:40:12 -0400

    Add list_active_member_outreach

---
+++ R/list_active_member_outreach.R
@@ -0,0 +1,36 @@
+#' Active Member Outreach List
+#' 
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr arrange
+#' 
+#' @return a flextable showing active members with phone and email who haven't been seen in more than a day
+#' @export
+#'
+#' @examples
+#' list_active_member_outreach()
+
+list_active_member_outreach <- function() {
+    
+    c2_groups=Name=Since=da_date_of_attendance=MostRecent=`Most Recent`=id=last_name=first_name=Phone=c2_primary_phone=Email=email_address=NULL
+    
+    contacts <- get_contacts() %>%
+        dplyr::filter(grepl("Member", c2_groups)) %>%
+        dplyr::filter(!grepl("Deceased", c2_groups)) %>%
+        dplyr::mutate(Name = paste(last_name, first_name, sep = ", ")) %>%
+        dplyr::inner_join(get_attendance(days=90), by = c("id" = "contact_2_id")) %>%
+        dplyr::mutate(Since = Sys.Date() - as_date(da_date_of_attendance)) %>%
+        dplyr::group_by(id, Name) %>%
+        dplyr::summarise(MostRecent = min(Since), c2_primary_phone = min(c2_primary_phone), email_address = min(email_address)) %>%
+        dplyr::filter(MostRecent > 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::arrange(MostRecent, Name) %>%
+        dplyr::select(Name,  `Most Recent` = MostRecent, Phone = c2_primary_phone, Email = email_address)
+    
+    ft <- goc_table(contacts, paste("Active Member Outreach for", Sys.Date()))
+    ft
+}
