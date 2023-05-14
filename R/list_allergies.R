1e0a4e49921b8ef357eb109ad0ca88b7d21f731e
Mike Conlon <mconlon@ufl.edu>
2023-05-13 20:06:11 -0400

    Add stringr declaration

--- R/list_allergies.R
+++ R/list_allergies.R
@@ -4,6 +4,8 @@
 #' @importFrom dplyr mutate
 #' @importFrom dplyr arrange
 #' @importFrom dplyr select
+#' @importFrom stringr str_detect
+
 #' @return a flextable list of members and their allergies
 #' @export
 #'
@@ -15,7 +17,8 @@ list_allergies <- function() {
     flo_allergies=last_name=first_name=NULL
     
     allergies <- get_members() %>%
-        dplyr::filter(!str_detect(flo_allergies, "unknown") & !str_detect(flo_allergies, "none") & flo_allergies != "" & !is.na(flo_allergies)) %>%
+        dplyr::filter(!stringr::str_detect(flo_allergies, "unknown") & !stringr::str_detect(flo_allergies, "none") & 
+                          flo_allergies != "" & !is.na(flo_allergies)) %>%
         dplyr::mutate(flo_allergies = gsub(",", "A", flo_allergies)) %>%
         dplyr::mutate(flo_allergies = gsub("_", "B", flo_allergies)) %>%
         dplyr::mutate(flo_allergies = gsub("[[:punct:]]", "", flo_allergies)) %>%
