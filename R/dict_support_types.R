dcc8c3ae56294a1dcd1e7738ec3c1448fcc1ea94
Mike Conlon <mconlon@ufl.edu>
2023-05-23 17:59:53 -0400

    Add list_supports_by_year.  Improve labels in dict_support_types

--- R/dict_support_types.R
+++ R/dict_support_types.R
@@ -11,9 +11,9 @@ dict_support_types <- function() {
     vars <- list(
         c("Benefits", "Benefits", "Bene\nfits"),
         c("Culinary", "Culinary", "Culi\nnary"),
-        c("Education_in_house", "Education -- in-house", "Edu\nGOC"),
-        c("Education_pre_enrollment", "Education -- pre-enrollment", "Edu\npre"),
-        c("Education_post_enrollment", "Education -- post-enrollment", "Edu\npost"),
+        c("Education_in_house", "Education -- In-house", "Edu\nGOC"),
+        c("Education_pre_enrollment", "Education -- Pre-enrollment", "Edu\npre"),
+        c("Education_post_enrollment", "Education -- Post-enrollment", "Edu\npost"),
         c("Employment_readiness", "Employment readiness", "Emp\npre"),
         c("Employment_IE", "Employment -- Independent", "Emp\nIE"),
         c("Employment_SE", "Employment -- Supplemental", "Emp\nSE"),
@@ -28,14 +28,14 @@ dict_support_types <- function() {
         c("Referral", "Referral", "Ref\nerral"),
         c("Social", "Social", "Soc\nial"),
         c("Spiritual", "Spritiual", "Spiri\ntual"),
-        c("Transportation_education", "Transportation -- education", "Van\nEdu"),
+        c("Transportation_education", "Transportation -- Education", "Van\nEdu"),
         c("Transportation_employment", "Transportation -- Employment", "Van\nEmp"),
         c("Transportation_goc", "Transportation -- GOC", "Van\nGOC"),
         c("Transportation_health", "Transportation -- Health", "Van\nHlth"),
         c("Transportation_wellness", "Transportation -- Wellness", "Van\nWell"),
         c("Unknown", "Unknown", "Unk"),
         c("Wellness", "Wellness", "Well\nness"),
-        c("Work_ordered_day", "Word Ordered Day", "WOD")
+        c("Work_ordered_day", "Work Ordered Day", "WOD")
     )
     
    data.frame(key = sapply(vars, function(x) x[1]), value = sapply(vars, function(x) x[2]), narrow = sapply(vars, function(x) x[3]))
---
+++ R/list_supports_by_year.R
@@ -0,0 +1,47 @@
+#' Supports by Year
+#' 
+#' @importFrom dplyr group_by
+#' @importFrom dplyr tally
+#' @importFrom dplyr select
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr tally
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr summarise
+#' @importFrom dplyr across
+#' @importFrom tidyr pivot_wider
+#' @importFrom lubridate year
+#'
+#' @return a flextable showing support types (rows) and years (columns).  Entries are the counts of supports of that type for the year.
+#' @export
+#'
+#' @examples
+#' list_supports_by_year()
+
+list_supports_by_year <- function() {
+    
+    Year=flo_support_date=flo_support_type=value=NULL
+    
+    supports <- get_supports() %>%
+        dplyr::mutate(Year = lubridate::year(flo_support_date)) %>%
+        dplyr::select(flo_support_type, Year) %>%
+        dplyr::inner_join(dict_support_types(), by = c("flo_support_type" = "key")) %>%
+        dplyr::group_by(value, Year) %>%
+        dplyr::tally() %>%
+        tidyr::pivot_wider(names_from=Year, values_from=n) %>%
+        dplyr::mutate(across(where(is.numeric), ~ ifelse(is.na(.), 0, .))) %>%
+        dplyr::ungroup()
+    
+    supports <- supports %>%
+        dplyr::select(`Support Type`=value, order(colnames(supports)))
+    
+    supports <- dplyr::bind_rows(supports, dplyr::summarise(supports,dplyr::across(where(is.numeric), sum), 
+                                                   dplyr::across(where(is.character), ~"Total")))
+    
+    supports[supports==0] <- NA
+    
+    ft <- goc_table(supports, "Supports Types By Year")
+    ft
+}
