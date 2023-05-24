4bc6eef086fd65ee143a95a38c434f9f16496844
Mike Conlon <mconlon@ufl.edu>
2023-05-23 21:49:27 -0400

    Add goc_plot, plot_members_by_year

---
+++ R/goc_plot.R
@@ -0,0 +1,26 @@
+#' GOC Plot
+#'
+#' @param p a plot to be goc-styled
+#' 
+#' @importFrom ggplot2 theme_linedraw
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#'
+#' @return a plot that is goc-styled
+#' @export
+#'
+#' @examples
+#' d <- data.frame(x=1:5, y=4:8)
+#' goc_plot(ggplot2::ggplot(d, ggplot2::aes(x,y)) + ggplot2::geom_line())
+
+goc_plot <- function(p) {
+    p + ggplot2::theme_linedraw() +
+        ggplot2::theme(plot.title    = ggplot2::element_text(hjust = 0.5, family = "sans",
+                                                             size = 32)) +
+        ggplot2::theme(plot.subtitle = ggplot2::element_text(hjust = 0.5, family = "serif", 
+                                                             size = 11, face="italic")) +
+        ggplot2::theme(axis.title    = ggplot2::element_text(hjust = 0.5, family = "sans",
+                                                             size = 11, face = "bold")) +
+        ggplot2::theme(axis.text     = ggplot2::element_text(hjust = 0.5, family = "sans",
+                                                             size = 11))
+}
---
+++ R/plot_members_by_year.R
@@ -0,0 +1,22 @@
+#' Plot Members By Year
+#'
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 geom_line
+#'
+#' @return a plot of members by year
+#' @export
+#'
+#' @examples
+#' plot_members_by_year()
+
+plot_members_by_year <- function() {
+    
+    Year=`Number of Members`=NULL
+    
+    goc_plot(ggplot2::ggplot(list_members_by_year(output="data.frame"), ggplot2::aes(Year, `Number of Members`)) +
+        ggplot2::ggtitle("Members by Year") +
+        ggplot2::geom_line())
+    
+}
