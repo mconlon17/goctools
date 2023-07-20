c030e3ac7617fa7adcaa1c9d49ff84959cebfc39
Mike Conlon <mconlon@ufl.edu>
2023-07-19 21:02:59 -0400

    Restore flourish_connection

---
+++ R/flourish_connection.R
@@ -0,0 +1,20 @@
+#' Flourish Connection
+#'
+#' @importFrom RMySQL dbConnect
+#'
+#' @return a database connection to Flourish
+#' @export
+#'
+#' @examples
+#' con <- flourish_connection()
+#' RMySQL::dbDisconnect(con)
+
+flourish_connection <- function() {
+    RMySQL::dbConnect(RMySQL::MySQL(),
+                      dbname   = keyring::key_get("Flourish GOC production dbname"),
+                      host     = keyring::key_get("Flourish GOC production host"),
+                      port     = 3306,
+                      user     = keyring::key_get("Flourish GOC production user"),
+                      password = keyring::key_get("Flourish GOC production password")
+    )
+}
